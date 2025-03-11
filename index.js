3a2f10e8f1f05850591ac3b7e084249aa2f7a4a8
Sekaivi <rouibahmayssa@gmail.com>
2025-03-11 22:35:31 +0100

    packages

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
 const { readFile } = require("fs/promises");
 const path = require("path");
 const PORT = 80;
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
 app.use(express.static(path.join(__dirname, "public")));
-
-
-app.listen(PORT, () => {
-    console.log("Server running on http://localhost:" + PORT);
-});
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "html/index.html"));
@@ -17,4 +17,25 @@ app.get("/", (req, res) => {
 
 app.get("/game", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "html/Testgame.html"));
+});
+
+
+/* DEMARRAGE DU SERVEUR */
+server.listen(PORT, () => {
+    console.log("Server running on http://localhost:" + PORT);
+});
+
+/* DEBUG */
+
+io.on("connection", (socket) => {
+    console.log("Un utilisateur s'est connecté");
+
+    socket.on("message", (msg) => {
+        console.log("Message reçu :", msg);
+        io.emit("message", msg); // Répète le message à tous les clients
+    });
+
+    socket.on("disconnect", () => {
+        console.log("Un utilisateur s'est déconnecté");
+    });
 });
