402f05a1f0217e89cfa71cd2e73923778b9d13d8
Sekaivi <rouibahmayssa@gmail.com>
2025-03-11 15:29:34 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const { readFile } = require("fs/promises");
+const path = require("path");
+const PORT = 80;
+
+const app = express();
+app.use(express.static(path.join(__dirname, "public")));
+
+
+app.listen(PORT, () => {
+    console.log("Server running on http://localhost:" + PORT);
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "html/index.html"));
+});
+
+app.get("/game", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "html/Testgame.html"));
+});
